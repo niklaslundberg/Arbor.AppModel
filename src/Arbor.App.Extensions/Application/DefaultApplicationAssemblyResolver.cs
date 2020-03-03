629cf06917c1da43c4d305102d325835bacaed59
Niklas Lundberg <info@nlundberg.se>
2020-03-03 16:50:55 +0100

    Added assembly resolver interface and default implementation.

---
+++ src/Arbor.App.Extensions/Application/DefaultApplicationAssemblyResolver.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Immutable;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace Arbor.App.Extensions.Application
+{
+    public class DefaultApplicationAssemblyResolver : IApplicationAssemblyResolver
+    {
+        public Task<ImmutableArray<Assembly>>
+            GetAssemblies(string[] assemblyNameStartsWith, bool cacheEnabled = true) =>
+            Task.FromResult(ApplicationAssemblies.FilteredAssemblies(assemblyNameStartsWith, cacheEnabled));
+    }
+}
---
+++ src/Arbor.App.Extensions/Application/IApplicationAssemblyResolver.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Immutable;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace Arbor.App.Extensions.Application
+{
+    public interface IApplicationAssemblyResolver
+    {
+        Task<ImmutableArray<Assembly>> GetAssemblies(string[] assemblyNameStartsWith,bool cacheEnabled = true);
+    }
+}
