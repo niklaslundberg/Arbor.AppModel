e52498f1e130d357d4426212a5ec0e55502cc869
Niklas Lundberg <info@nlundberg.se>
2020-06-26 14:28:37 +0200

    Added http logging filter.

---
+++ src/Arbor.App.Extensions/Http/CustomLoggingFilter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using JetBrains.Annotations;
+using Microsoft.Extensions.Http;
+
+namespace Arbor.App.Extensions.Http
+{
+    [UsedImplicitly]
+    public class CustomLoggingFilter : IHttpMessageHandlerBuilderFilter
+    {
+        public Action<HttpMessageHandlerBuilder> Configure(Action<HttpMessageHandlerBuilder> next)
+        {
+            if (next is null)
+            {
+                throw new ArgumentNullException(nameof(next));
+            }
+
+            return builder => { };
+        }
+    }
+}
---
+++ src/Arbor.App.Extensions/Http/HttpLoggingConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Text;
+using Arbor.App.Extensions.Configuration;
+using Arbor.KVConfiguration.Core.Metadata;
+using Arbor.KVConfiguration.Urns;
+using JetBrains.Annotations;
+
+namespace Arbor.App.Extensions.Http
+{
+    [UsedImplicitly]
+    [Urn(HttpLoggingConfigurationUrn)]
+    public class HttpLoggingConfiguration : IConfigurationValues
+    {
+        [Metadata(defaultValue: "true")]
+        public const string HttpLoggingEnabled = "urn:arbor:app:http:http-logging:default:enabled";
+
+        [Metadata] public const string HttpLoggingConfigurationUrn = "urn:arbor:app:http:http-logging";
+
+        public HttpLoggingConfiguration(bool enabled) => Enabled = enabled;
+
+        public bool Enabled { get; }
+    }
+}
