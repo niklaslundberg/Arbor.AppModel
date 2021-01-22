24d1a7dad7cd2a567da841d29c291ac27bbbc36c
Niklas Lundberg <info@nlundberg.se>
2021-01-22 11:55:10 +0100

    Updated build script.

--- src/Arbor.App.Extensions/Logging/LogEventLevelExtensions.cs
+++ src/Arbor.App.Extensions/Logging/LogEventLevelExtensions.cs
@@ -6,10 +6,10 @@ namespace Arbor.App.Extensions.Logging
     public static class LogEventLevelExtensions
     {
         public static LogEventLevel ParseOrDefault(
-            this string levelText,
+            this string? levelText,
             LogEventLevel level = LogEventLevel.Information)
         {
-            if (levelText.IsNullOrWhiteSpace())
+            if (string.IsNullOrWhiteSpace(levelText))
             {
                 return level;
             }
