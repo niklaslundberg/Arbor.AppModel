f5ad62dddf7ab18aabea2c74a0b57b3aaafd62ef
Niklas Lundberg <info@nlundberg.se>
2021-05-04 18:40:08 +0200

    Added versioned distributed cache extensions.

---
+++ src/Arbor.App.Extensions/Caching/CurrentCacheVersion.cs
@@ -0,0 +1,7 @@
+﻿namespace Arbor.App.Extensions.Caching
+{
+    public class CurrentCacheVersion
+    {
+        public CacheVersion CurrentVersion { get; set; }
+    }
+}
---
+++ src/Arbor.App.Extensions/Caching/DistributedCacheExtensions.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
+
+namespace Arbor.App.Extensions.Caching
+{
+    public static class DistributedCacheExtensions
+    {
+        public static async Task SetWithVersionAsync<T>(this IDistributedCache cache, string key, [NotNull] T item,
+            CacheVersion cacheVersion, CancellationToken cancellationToken = default) where T : class
+        {
+            if (cache == null)
+            {
+                throw new ArgumentNullException(nameof(cache));
+            }
+
+            if (key == null)
+            {
+                throw new ArgumentNullException(nameof(key));
+            }
+
+            if (item == null)
+            {
+                throw new ArgumentNullException(nameof(item));
+            }
+
+            string? json =
+                JsonConvert.SerializeObject(new VersionedJson<T> {Instance = item, Version = cacheVersion.Version});
+
+            await cache.SetStringAsync(key, json, cancellationToken);
+        }
+
+        public static async Task<T?> GetWithVersionAsync<T>(this IDistributedCache cache, string key,
+            CacheVersion cacheVersion, CancellationToken cancellationToken = default) where T : class
+        {
+            if (cache == null)
+            {
+                throw new ArgumentNullException(nameof(cache));
+            }
+
+            if (key == null)
+            {
+                throw new ArgumentNullException(nameof(key));
+            }
+
+            try
+            {
+                string? json = await cache.GetStringAsync(key, cancellationToken);
+
+                if (string.IsNullOrWhiteSpace(json))
+                {
+                    return null;
+                }
+
+                var versionedObject = JsonConvert.DeserializeObject<VersionedJson<T>>(json);
+
+                if (versionedObject.Version == cacheVersion.Version)
+                {
+                    return versionedObject.Instance;
+                }
+
+                await cache.RemoveAsync(key, cancellationToken);
+            }
+            catch (Exception)
+            {
+                // ignore serialization issues
+                return null;
+            }
+
+            return null;
+        }
+
+        private class VersionedJson<T>
+        {
+            public int Version { get; set; }
+            public T? Instance { get; set; }
+        }
+    }
+}
